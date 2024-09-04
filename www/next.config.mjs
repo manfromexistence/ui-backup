76d6d25e0b8c6042a1f3afed14106d180a858d89
manfromexistence <ajju40959@gmail.com>
2024-09-04 10:15:20 +0000

    feat: hello

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -3,7 +3,7 @@ import { createContentlayerPlugin } from "next-contentlayer2"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   images: {
     remotePatterns: [
       {
